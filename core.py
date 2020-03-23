ac7fb59312c92cd3b2dd2a498f4fe8e15742e940
NoMercy <qawedytui@gmail.com>
2020-03-23 12:54:56 +0200

    Added create_wpkn_table function and changed furies function to use wpkn table.

--- core.py
+++ core.py
@@ -18,22 +18,35 @@ def signal(the_n, the_upper_n, the_w_max):
     return cont
 
 
-def furies(the_signal, the_upper_n):
+def furies(the_signal, the_upper_n, the_wpkn_table):
     result = []
     for p in range(the_upper_n):
         real_part = 0
         imaginary_part = 0
         for k in range(the_upper_n):
-            real_part += the_signal[k]*math.cos((2*math.pi*p*k)/the_upper_n)
-            imaginary_part += the_signal[k] * math.sin((2 * math.pi * p * k) / the_upper_n)
+            real_part += the_signal[k] * the_wpkn_table[p][k][0]
+            imaginary_part += the_signal[k] * the_wpkn_table[p][k][1]
         result.append(math.sqrt(real_part**2 + imaginary_part**2))
     return result
+
+
+def create_table_wpkn(the_upper_n):
+    wpkn_table = []
+    for p in range(the_upper_n):
+        row = []
+        for k in range(the_upper_n):
+            re_w = math.cos((2*math.pi*p*k)/the_upper_n)
+            im_w = math.cos((2*math.pi*p*k)/the_upper_n)
+            row.append((re_w, im_w))
+        wpkn_table.append(row)
+    return wpkn_table
 
 
 # Graph 1 -----------------
 n, w_max, N = 12, 2700, 64
 sig = signal(n, N, w_max)
-fur = furies(sig, N)
+wpkn_tab = create_table_wpkn(N)
+fur = furies(sig, N, wpkn_tab)
 
 plt.bar([i for i in range(N)], fur)
 plt.title('Lab3')
